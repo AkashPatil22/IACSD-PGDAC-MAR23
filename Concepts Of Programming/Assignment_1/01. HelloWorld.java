e812b319c2918f1e5d785bb67635258890041e58
AkashPatil22 <131249297+AkashPatil22@users.noreply.github.com>
2023-05-20 19:59:27 +0530

    Add files via upload

---
+++ Concepts Of Programming/Assignment_1/01. HelloWorld.java
@@ -0,0 +1,8 @@
+package ConceptsOfProgramming.Assignment_1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //printing output
+        System.out.println("Hello World");
+    }
+}
